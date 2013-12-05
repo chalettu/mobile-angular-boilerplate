782d71ed4167529d925fce438e36f9d8fd97940f
Chris Hale <ChrisHale@Chriss-MacBook-Air-2.local>
2013-12-04 21:31:46 -0600

    Added base libraries to boilerplate

---
+++ css/css_includes.js
@@ -0,0 +1,6 @@
+var css_files=['css/bootstrap.min.css','css/bootstrap-theme.min.css'];
+
+for (var i=0;i< css_files.length;i++)
+{
+	head.load(css_files[i]);
+}
---
+++ js/js_includes.js
@@ -0,0 +1,6 @@
+head.js(
+	{angular:"js/libs/angular.min.js" },
+	{jq: "js/libs/jquery-2.0.3.min.js"},
+	{bootstrap: "js/libs/bootstrap.min.js"},
+	{parsley: "js/libs/parsley.min.js"}
+);
